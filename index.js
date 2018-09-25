3532c22e379f1d1fc5b6ccf4b7cb9ea420e14fac
sand-enqos <sandeepp@enqos.com>
2018-09-25 09:15:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+   host: 'redis-server',
+   port: 6379
+});
+
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: 'mysql-server',
+  user: 'root',
+  password: 'root',
+  database: 'flvservice'
+});
+
+var sqlStatus = "not connected";
+connection.connect(function(err) {
+    if (err) throw err
+        console.log('You are now connected...');
+        sqlStatus = "connected";   
+});
+  
+
+var count = 0;
+app.get('/', (req, res) => {
+   client.set('visits', count++);
+   client.get('visits', (err, val) => {
+       res.send(`this is the count test ${val} -- ${sqlStatus}`);
+   });
+});
+
+app.listen(3000, () =>{
+    console.log('Listening ports');
+});
+
+
+
+
+
